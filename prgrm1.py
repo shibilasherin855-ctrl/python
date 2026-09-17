63f1fb2b3188618c419736ee0fcd9bae7ed18f85
sadha-kp <sadhakp2003@gmail.com>
2026-09-17 15:28:43 +0530

    my first commit

---
+++ prgrm1.py
@@ -0,0 +1,4 @@
+First_name=input("enter your first_name:")
+Last_name=input("enter your last_name:")
+print("Greeting!!!",First_name,Last_name)
+
---
+++ prgrm2.py
@@ -0,0 +1,6 @@
+a=10
+b=10.5
+c=2+3;
+print("integer:",a)
+print("float:",b)
+print("complex:",c)

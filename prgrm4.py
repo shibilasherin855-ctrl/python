7529f86cd60abb087148b0cecd2a038c403a24ee
shibilasherin855-ctrl <shibilasherin855@gmail.com>
2026-09-22 15:35:17 +0530

    new

---
+++ prgrm4.py
@@ -0,0 +1,9 @@
+basic_pay=float(input("enter the basic pay:"))
+HRA=10/100*basic_pay
+TA=5/100*basic_pay
+Salary=basic_pay+HRA+TA
+print("Basic Pay=",basic_pay)
+print("HRA=",HRA)
+print("TA=",TA)
+print("Total Salary=",Salary)
+
